5a8ee47c734373232c887d4d33575a1ea51b5787
REmyJRD <remyjardinpro@gmail.com>
2022-10-21 11:46:57 +0200

    Fill and RandomGen

--- main.c
+++ main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <stdlib.h>
 
-int main() {
-    printf("test");
-    VRDR
+int randgen()
+{
+    int r = rand();
+    return r;
+}
+
+int *createarray()
+{
+    int *array;
+    int i;
+    array = malloc(10 * sizeof(int));
+    for (i = 0; i < 10; i++)
+    {
+        array[i] = randgen();
+    }
+    return array;
+}
+
+int main()
+{
+    srand(time(0));
+    int i;
+
+    printf("--- Creation tableaux... --- \n");
+    int *array = createarray();
+    printf("-- Affichage Tableau --\n");
+    for (i = 0; i < 10; i++)
+    {
+        printf(" - %d | %d", array[i], i);
+    }
 }
